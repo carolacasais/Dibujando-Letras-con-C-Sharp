aa4890e4371eda34a2541b69a2622a7daa4cbd56
Carola Casais <carola.casais@gmail.com>
2020-10-09 20:49:53 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace alfabeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("LETRA G:");
+            int length = 7;
+            string asterisco = "*";
+            string blanco = " ";
+            for (int row = 0; row < length; row++)
+            {
+                for (int column = 0; column < length; column++)
+
+                {
+                    Console.Write(
+                        column == 0 ||
+                        (column == 6 && row > 2) ||
+                        (row == 3 && column > 3) ||
+                        row == 0 ||
+                        row == 6
+                        ? asterisco : blanco);
+                }
+                Console.WriteLine(" ");
+            }
+
+
+            Console.WriteLine("LETRA F:");
+            for (int row = 0; row < length; row++)
+            {
+                for (int column = 0; column < length; column++)
+                {
+                    Console.Write(
+                        row == 0 ||
+                        row == 3 ||
+                        column == 0
+                        ? asterisco : blanco);
+                }
+                Console.WriteLine(" ");
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+***Dibujar el Abecedario***
+
+A continuación se muestra un programa C# que dibuja las letras del abecedario en una matriz.
+En el ejemplo dibijamos las letras "G" y "F".
